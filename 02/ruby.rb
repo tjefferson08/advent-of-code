a72b755349dd88ffcf59ae711fa5d44004758404
Travis Jefferson <tjefferson@sharethrough.com>
2017-12-06 10:38:12 -0600

    Yucky phase 2 for day 2, but it works!

--- 02/ruby.rb
+++ 02/ruby.rb
@@ -14,12 +14,40 @@ def checksum(raw_input)
   end.reduce(:+)
 end
 
+def checksum_phase_2(raw_input)
+  raw_input.gsub!(/\r\n?/, "\n")
+  raw_input.split("\n").map do |line|
+    row_numbers = line.split.map(&:strip).map(&:to_i)
+    find_evenly_divisible_pair_and_divide(row_numbers)
+  end.reduce(:+)
+end
+
+def find_evenly_divisible_pair_and_divide(num_arr)
+  for i in (0..num_arr.length - 1)
+    for j in ((i + 1)..num_arr.length - 1)
+      puts "comparing indices #{i}, #{j}"
+      if num_arr[i] % num_arr[j] == 0
+        puts "found the pair! #{num_arr[i]}, #{num_arr[j]}"
+        return num_arr[i] / num_arr[j]
+      elsif num_arr[j] % num_arr[i] == 0
+        puts "found the pair! #{num_arr[i]}, #{num_arr[j]}"
+        return num_arr[j] / num_arr[i]
+      end
+    end
+  end
+end
+
 text=File.open('input.txt').read
 
 puts "soln: #{checksum(text)}"
+puts "soln phase 2: #{checksum_phase_2(text)}"
 
 class TestMeme < Minitest::Test
   def test_things
     assert_equal 18, checksum("5 1 9 5\n7 5 3\n2 4 6 8\n")
+  end
+
+  def test_things_phase_2
+    assert_equal(9, checksum_phase_2("5 9 2 8\n9 4 7 3\n3 8 6 5\n"))
   end
 end
