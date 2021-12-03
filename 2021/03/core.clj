2a4a145b661d45bd9de5098c05fbd82eacf65e3e
Travis Jefferson <traviscjefferson@gmail.com>
2021-12-03 15:48:28 -0500

    Ya gotta narrow the gamma/eps calc each loop

--- 2021/03/core.clj
+++ 2021/03/core.clj
@@ -1,11 +1,59 @@
+(def FILENAME "./input.txt")
+;; (def FILENAME "./simple_input.txt")
 
 (defn input->bit-colls []
-  (->> (line-seq (io/reader "./input.txt"))
+  (->> (line-seq (io/reader FILENAME))
     (map #(str/split % #""))
     (map (fn [digits] (map #(Integer/parseInt %) digits)))))
 
+(def INPUT_SIZE 1000)
+
 (defn bit-counts [bit-colls]
   (apply map + bit-colls))
+
+(defn gamma-rate [bit-colls]
+  (->> (bit-counts bit-colls)
+       (map #(if (>= % (/ (count bit-colls) 2))
+               1
+               0))))
+
+;; this is just the inverse of gamma-rate
+(defn epsilon-rate [bit-colls]
+  (->> (bit-counts bit-colls)
+       (map #(if (>= % (/ (count bit-colls) 2))
+               0
+               1))))
+
+(defn bit-coll->dec [coll]
+  (Integer/parseInt (str/join "" coll ) 2))
+
+;; TODO recalc gamma-col with new data whoops
+(defn oxygen-generator [bit-colls]
+  (loop [remaining-colls bit-colls
+         gamma-idx 0]
+    (let [gamma-coll (gamma-rate remaining-colls)
+          narrowed-colls (filter
+                           #(= (nth % gamma-idx) (nth gamma-coll gamma-idx))
+                           remaining-colls)]
+      ;; (println "filtered by " (nth gamma-coll gamma-idx))
+      ;; (println "narrowed:  " (count narrowed-colls) narrowed-colls)
+      (if (= 1 (count narrowed-colls))
+          (first narrowed-colls)
+          (recur narrowed-colls (inc gamma-idx))))))
+
+(defn co2-scrubber [bit-colls]
+  (loop [remaining-colls bit-colls
+         eps-idx 0]
+    (let [eps-coll (epsilon-rate remaining-colls)
+          narrowed-colls (filter
+                           #(= (nth % eps-idx) (nth eps-coll eps-idx))
+                           remaining-colls)]
+      (println "filtered by " (nth eps-coll eps-idx))
+      (println "narrowed:  " (count narrowed-colls) narrowed-colls)
+      (if (= 1 (count narrowed-colls))
+          (first narrowed-colls)
+          (recur narrowed-colls (inc eps-idx))))))
+
 
 (comment
 
@@ -18,6 +66,29 @@
 
   (take 5 (input->bit-colls))
 
+  (bit-counts (input->bit-colls))
 
 
-  nil)
+  (->>
+   (take 3 (input->bit-colls))
+   (gamma-rate)
+   (bit-coll->dec))
+
+  (->>
+   (take 3 (input->bit-colls))
+   epsilon-rate)
+
+  (let [gamma (gamma-rate (input->bit-colls))
+        eps   (epsilon-rate (input->bit-colls))]
+   (* (bit-coll->dec gamma) (bit-coll->dec eps)))
+
+  ;; Part 2
+  ;;
+  (assert (= (gamma-rate (input->bit-colls))
+             [1 0 1 1 0]))
+
+  (*
+    (oxygen-generator (input->bit-colls))
+    (co2-scrubber (input->bit-colls)))
+
+ nil)
