baca211c55467d103e6717ff185646ee8d5a792a
Travis Jefferson <traviscjefferson@gmail.com>
2021-12-12 10:34:26 -0500

    Visited -> map, fix test data bug

--- 2021/12/core.clj
+++ 2021/12/core.clj
@@ -17,16 +17,19 @@
 
 
 (defn paths
-  ([g] (paths g "start" "end" #{}))
+  ([g] (paths g "start" "end"))
+  ([g start stop] (paths g start stop (zipmap (keys g) (repeat 0))))
   ([g start stop visited]
-   (println start stop visited)
-   (let [next-candidates (set/difference (g start) visited)
-         visited'        (conj visited start)]
+   (let [next-candidates (set/difference (g start) (set (filter #(pos? (visited %)) (keys visited))))
+         visited'        (update visited start inc)]
+     (println "start stop visited" start stop visited)
+     (println "next-candidates, visited'" next-candidates visited')
      (cond (= start stop)           (list (list stop))
            (empty? next-candidates) '()
            :else (reduce
                   (fn [acc next-start]
                     (->> (paths g next-start stop visited')
+                         ((fn [el] (println "reducing" el) el))
                          (map #(conj % start))
                          (concat acc)))
                   '()
@@ -47,8 +50,10 @@
   (update {} :b conj :a)
 
   (paths state)
-  (paths {:a #{:b} :b #{:a}} :a :b #{})
-  (paths {:a #{:b} :b #{:d :a} :d #{:b}} :a :d #{})
-  (paths {:a #{:b} :b #{:a :d :c} :c #{:e} :d #{:e}} :a :e #{})
+  (paths {:a #{:b} :b #{:a}} :a :b)
+  (paths {:a #{:b} :b #{:d :a} :d #{:b}} :a :d)
+  (paths {:a #{:b} :b #{:a :d :c} :c #{:e} :d #{:e} :e #{:c :d}} :a :e)
+
+  (zipmap (keys state) (repeat 0))
 
   nil)
