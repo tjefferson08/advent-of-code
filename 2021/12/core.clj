5626852796431425667922ebf3920691584886ea
Travis Jefferson <traviscjefferson@gmail.com>
2021-12-12 11:34:06 -0500

    part one doooooone woot

--- 2021/12/core.clj
+++ 2021/12/core.clj
@@ -1,6 +1,7 @@
-;; (def FILENAME "./input.txt")
-(def FILENAME "./simple_input.txt")
-;; (def FILENAME "./super_simple_input.txt")
+(def FILENAME "./input.txt")
+;; (def FILENAME "./simple_input.txt")
+;; (def FILENAME "./ex3.txt")
+;; (def FILENAME "./ex2.txt")
 
 (defn map-kv [m f]
   (into {} (for [[k v] m] [k (f v)])))
@@ -9,32 +10,37 @@
                 (map #(str/split % #"-"))
                 (reduce
                   (fn [acc [a b]]
-                     (let [append (fn [old new] (if old (conj old new) #{}))]
-                       (-> acc
-                        (update a append b)
-                        (update b append a))))
+                    (let [append (fn [old new]
+                                   (if old (conj old new) #{new}))]
+                      (-> acc
+                       (update a append b)
+                       (update b append a))))
                   {})))
 
+
+(defn visitable-nodes [g visited start]
+  (->> (g start)
+       (filter #(if (= (str/upper-case %) %)
+                  true
+                  (zero? (visited %))))))
 
 (defn paths
   ([g] (paths g "start" "end"))
   ([g start stop] (paths g start stop (zipmap (keys g) (repeat 0))))
   ([g start stop visited]
-   (let [next-candidates (set/difference (g start) (set (filter #(pos? (visited %)) (keys visited))))
+   (let [next-candidates (visitable-nodes g visited start)
          visited'        (update visited start inc)]
-     (println "start stop visited" start stop visited)
-     (println "next-candidates, visited'" next-candidates visited')
+     ;; (println "start stop visited" start stop visited)
+     ;; (println "next-candidates" next-candidates)
      (cond (= start stop)           (list (list stop))
            (empty? next-candidates) '()
            :else (reduce
                   (fn [acc next-start]
                     (->> (paths g next-start stop visited')
-                         ((fn [el] (println "reducing" el) el))
                          (map #(conj % start))
                          (concat acc)))
                   '()
                   next-candidates)))))
-
 
 (comment
 
@@ -55,5 +61,17 @@
   (paths {:a #{:b} :b #{:a :d :c} :c #{:e} :d #{:e} :e #{:c :d}} :a :e)
 
   (zipmap (keys state) (repeat 0))
+
+  (str/upper-case "a")
+
+  (visitable-nodes {"a" #{"B"} "B" #{"a" "D" "c"} "c" #{"e"} "D" #{"e"} "e" #{"c" "D"}}
+                   {"a" 1 "B" 10 "c" 0 "D" 3 "e" 0}
+                   "B")
+
+  (paths {"a" #{"b"} "b" #{"a" "d" "C"} "C" #{"e"} "d" #{"e"} "e" #{"C" "d"}} "a" "e")
+
+  (paths state)
+
+  (count (paths state))
 
   nil)
