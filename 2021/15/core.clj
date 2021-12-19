ad6b430328e2bca20a6e8282f495887da09499ea
Travis Jefferson <traviscjefferson@gmail.com>
2021-12-18 21:32:29 -0500

    Still can't get 15.2 :/

--- 2021/15/core.clj
+++ 2021/15/core.clj
@@ -4,11 +4,29 @@
 (def matrix (->> (line-seq (io/reader FILENAME))
                  (mapv (fn [l] (mapv #(Integer/parseInt %) (str/split l #""))))))
 
+(def small-matrix (->> (line-seq (io/reader FILENAME))
+                       (mapv (fn [l] (mapv #(Integer/parseInt %) (str/split l #""))))))
+
+(defn matrix-seq
+  ([] (matrix-seq small-matrix))
+  ([mtrx] (lazy-seq
+           (cons mtrx (matrix-seq
+                       (mapv (fn [row] (mapv #(if (= 9 %) 1 (inc %)) row)) mtrx))))))
+
+;; Large matrix for part 2
+(def matrix
+  (->> (partition 100 1 (matrix-seq))
+       (take 100)
+       (mapv #(into [] (apply concat %)))
+       (apply mapv concat)
+       (mapv #(into [] %))))
+
+;; (def GRID_SIZE (count matrix))
 (def GRID_SIZE (count matrix))
 
 (defn neighbors [[x y :as p]]
   (filterv (fn [[x' y']] (and (<= 0 x' (dec GRID_SIZE))
-                             (<= 0 y' (dec GRID_SIZE))))
+                              (<= 0 y' (dec GRID_SIZE))))
            [[(dec x) y]
             [(inc x) y]
             [x (inc y)]
@@ -19,29 +37,38 @@
         y (range GRID_SIZE)]
     [x y]))
 
-(def graph (reduce
-            (fn [g coord] (assoc g coord
-                                 {:adj (neighbors coord)
-                                  :risk (get-in matrix (reverse coord))}))
-            {}
-            (coord-seq matrix)))
+;; (def graph (reduce
+;;             (fn [g coord] (assoc g coord
+;;                                  {:adj (neighbors coord)
+;;                                   :risk (get-in matrix (reverse coord))}))
+;;             {}
+;;             (coord-seq matrix)))
 
-(defn traverse [graph start end]
+(defn traverse [matrix start end]
   (loop [unvisited (set (coord-seq matrix))
-         current start
-         distances (assoc (zipmap (keys graph) (repeat ##Inf)) start 0)]
-    (let [unvisited-neighbors (set/intersection (set (:adj (graph current))) unvisited)
-          tentative-distances (into {} (map (fn [n] [n (+ (:risk (graph n)) (distances current))]) unvisited-neighbors))]
-      (if (unvisited end)
-        (recur (disj unvisited current)
-               (first (sort-by distances unvisited))
-               (merge-with min distances tentative-distances))
-        distances))))
+         distances (assoc (zipmap (coord-seq matrix) (repeat ##Inf)) start 0)]
+    (if (unvisited end)
+      (let [current             (apply min-key distances unvisited)
+            ;; _                   (println "current" current)
+            unvisited'          (disj unvisited current)
+            nbors               (set (neighbors current))
+            unvisited-neighbors (set/intersection nbors unvisited')
+            tentative-distances (->> unvisited-neighbors
+                                     (map (fn [nbor] [nbor (+ (get-in matrix [(second nbor) (first nbor)]) (distances current))]))
+                                     (into {}))
+            ;; _                   (println "tent" tentative-distances)
+            distances' (merge-with min distances tentative-distances)]
+            ;; _                   (println "un-prime" unvisited')]
+        (recur unvisited'
+               distances'))
+     distances)))
 
 
 
+(time
+ (println ((traverse matrix [0 0] [499 499]) [499 499])))
 
-
+;; (time (println "hi" ((traverse matrix [0 0] [99 99]) [99 99])))
 
 (comment
 
@@ -57,9 +84,17 @@
 
   (get-in matrix [2 0])
 
-  (traverse graph [0 0] [99 99])
+  (traverse matrix [0 0] [0 0])
 
-  (set ((graph [0 1]) :adj))
+  ;; (set ((graph [0 1]) :adj))
+
+
+  (map first (take 3 (matrix-seq)))
+
+
+
+
+
 
 
   nil)
