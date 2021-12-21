de99769694ff6cabdc0c7afd368b9cafe9b1fa65
Travis Jefferson <travis@pathstream.com>
2021-12-20 22:10:54 -0500

    Day 20 works on sample input
    But not real input
    
    Has to do with how you map stuff at the border / infinity I think

--- 2021/20/core.clj
+++ 2021/20/core.clj
@@ -4,62 +4,81 @@
 (def enhancement-algo-ex (into [] (first (line-seq (io/reader "./simple_input.txt")))))
 (def enhancement-algo (into [] (first (line-seq (io/reader "./input.txt")))))
 (def image-seed-ex (mapv #(into [] %) (drop 2 (line-seq (io/reader "./simple_input.txt")))))
+(def image-seed-mp-ex (->> (line-seq (io/reader "./simple_input.txt"))
+                           (drop 2)
+                           (map-indexed (fn [y row] (map-indexed (fn [x el] [[x y] el]) row)))
+                           (apply concat)
+                           (into {})))
+(def image-seed-mp (->> (line-seq (io/reader "./input.txt"))
+                        (drop 2)
+                        (map-indexed (fn [y row] (map-indexed (fn [x el] [[x y] el]) row)))
+                        (apply concat)
+                        (into {})))
+
 (def image-seed (mapv #(into [] %) (drop 2 (line-seq (io/reader "./input.txt")))))
 
 (defn output-pixel [image [x y]]
-  (let [bits (for [x [(dec x) x (inc x)]
-                   y [(dec y) y (inc y)]]
-                [x y])]
-   (->> bits
-        (map #(get-in image (reverse %) \.))
-        (map {\. 0 \# 1})
-        (apply str)
-        ((fn [bin-str] (Long/parseLong bin-str 2))))))
+  (let [bits (for [y [(dec y) y (inc y)]
+                   x [(dec x) x (inc x)]]
+               [x y])]
+    (->> bits
+         (map #(get image % \.))
+         (map {\. 0 \# 1})
+         (apply str))))
+         ;; ((fn [bin-str] (Long/parseLong bin-str 2))))))
 
-(defn coords [image]
-  (let [size (count image)]
-    (for [x (range size)
-          y (range size)]
-     [x y])))
 
-(defn extended-coords [image]
-  (let [size (count image)]
-    (for [x (range (- 2) (+ size 2))
-          y (range (- 2) (+ size 2))]
-     [x y])))
+(defn output-coords [[x y :as p]]
+  (for [x (range (- x 2) (+ x 3))
+        y (range (- y 2) (+ y 3))]
+    [x y]))
 
 (defn transform [image enhancement]
-  (let [new-size (+ 4 (count image))]
-    (->> (extended-coords image)
-      (map #(vector % (get enhancement (output-pixel image %))))
-      (into {})
-      (reduce (fn [acc [k v]] (assoc-in acc k v)) (vec (repeat new-size (vec (repeat new-size \.))))))))
+  (->> (keys image)
+       (mapcat output-coords)
+       (map #(vector % (get enhancement (output-pixel image %))))
+       (into {})))
 
-(defn print-image [image]
-  (println "")
-  (->> (map (fn [row] (str (apply str row) "\n")) image)
-       println)
-  (println ""))
 
 (comment
   (+ 1 2)
 
-  (output-pixel image-seed-ex [2 1])
+  (count (output-coords [2 2]))
 
-  (get enhancement-algo-ex 34)
+  (->> [[0 2]]
+       (mapcat output-coords)
+       (map #(vector % (output-pixel image-seed-mp-ex %))))
 
-  (get-in image-seed-ex (reverse [2 3]))
+  (->> [[99 99]]
+       (mapcat output-coords)
+       (map #(vector % (output-pixel image-seed-mp %))))
 
-  (extended-coords image-seed-ex)
+  (get enhancement-algo-ex (output-pixel image-seed-mp-ex [2 2]))
+  (get enhancement-algo-ex (output-pixel image-seed-mp-ex [0 3]))
+  (output-pixel image-seed-mp-ex [0 3])
 
-  (into {} (map #(vector % (get-in image-seed-ex (reverse %))) (coords image-seed-ex)))
+  (get enhancement-algo-ex 192)
+  (get enhancement-algo 8)
 
-  (print-image (transform image-seed-ex enhancement-algo-ex))
+  (sort image-seed-mp)
 
+  (as-> image-seed-mp-ex _
+    (transform _ enhancement-algo-ex)
+    (transform _ enhancement-algo-ex)
+    (sort _))
+    ;; (filter (fn [[k v]] (= v \#)) _)
+    ;; (count _))
 
-  (vec (repeat 4 (vec (repeat 4 1))))
+  (as-> image-seed-mp _
+    (transform _ enhancement-algo)
+    (transform _ enhancement-algo)
+    (sort _))
+    ;; (filter (fn [[k v]] (= v \#)) _))
+    ;; (count _))
 
-  (assoc-in [[2 4 6]] [0 1] 10)
+    ;; (transform _ enhancement-algo-ex)
+    ;; (filter (fn [[k v]] (= v \#)) _)
+    ;; (count _))
 
 
   nil)
